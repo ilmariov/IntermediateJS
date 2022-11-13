0b24c32d1882b7d1f18c684d117e20eb05ba8ce3
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-11-12 23:37:13 -0500

    Create sumOddFibs.js

---
+++ sumOddFibs.js
@@ -0,0 +1,21 @@
+function sumFibs(num) {
+    const arrFibs = [1, 1];
+    let b1 = arrFibs[0];
+    let b2 = arrFibs[1];
+    let fib = b1 + b2;
+    if (num > 1) {
+        while (fib <= num) {
+            if (fib <= num) {arrFibs.push(fib)} 
+            b1 = b2;
+            b2 = fib;
+            fib = b1 + b2
+        }
+        return arrFibs
+            .filter(int => (int % 2) !== 0)
+            .reduce((a,b) => a + b, 0);
+    } else {
+        return num;
+    }
+}
+  
+console.log(sumFibs(75024));
