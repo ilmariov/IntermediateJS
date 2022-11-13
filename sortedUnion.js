ce73314b2287269b16adaa6b873de616f4be7d11
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-11-12 22:24:28 -0500

    Create sortedUnion.js

---
+++ sortedUnion.js
@@ -0,0 +1,14 @@
+function uniteUnique() {
+    const arr = Array.from(arguments);
+    const newArr = [];
+    for (let i=0; i<arr.length; i++) {
+        for (let j=0; j<arr[i].length; j++) {
+            if (!newArr.includes(arr[i][j])) {
+                newArr.push(arr[i][j])
+            }
+        }
+    }
+    return newArr;
+}
+  
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
